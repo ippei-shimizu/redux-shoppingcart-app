9ba350d79acaa64937fcf47324d2ec957a8fff83
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-03-18 15:59:08 +0900

    initialStateの定義

---
+++ src/features/cart/CartSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// 買い物かごの初期化 カート内で使用するものを定義する 動的に数値が変わるもの
+const initialState = {
+  cartItems: [],
+  amount: 0,
+  total: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {},
+});
