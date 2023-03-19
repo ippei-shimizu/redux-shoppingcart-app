e526c337b1fcddbe32699666a5fea0c37e0c8321
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-03-19 15:44:36 +0900

    モーダル実装

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import CartContainer from "./components/CartContainer";
+import Modal from "./components/Model";
 import NavBar from "./components/NavBar";
+import { calculateTotal } from "./features/cart/CartSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((state) => state.cart);
+  const { isOpen } = useSelector((state) => state.modal);
+  useEffect(() => {
+    dispatch(calculateTotal());
+  }, [cartItems, dispatch]);
+
   return (
     <main>
+      {isOpen && <Modal />}
       <NavBar />
       <CartContainer />
     </main>
